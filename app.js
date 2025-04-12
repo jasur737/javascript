fb095304efe0bd24451fc72f21204f7fd1ae34e7
jasur737 <baxtiyorovjasur728@gmail.com>
2025-04-12 22:00:21 +0500

    new project

---
+++ app.js
@@ -0,0 +1,28 @@
+let user = {
+    name:prompt ("what is your name?"),
+    age: 20,
+    isStudent: false,
+};
+user.country ="USA";
+console.log(user);
+
+// let stringValue = "!@#$%^&*()"
+// console.log("String:", stringValue);
+
+// let numberValue = 1274649249
+// console.log("Nuber:", numberValue);
+
+// let booleanValue = "false"
+// console.log("Boolean:", booleanValue);
+
+// let nullValue = null
+// console.log('Null', nullValue);
+
+// let undefinedValue = undefined
+// console.log("Undefined", undefinedValue);
+
+// let bigintValue = 33339758392224959559999n
+// console.log("Bigint", bigintValue);
+
+// let symbolValue = Symbol("hcybubu")
+// console.log("Symbol", symbolValue);
