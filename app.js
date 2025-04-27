dd3bdaa31eeb866253f730925b4a1dce0767d04f
jasur737 <baxtiyorovjasur728@gmail.com>
2025-04-27 22:42:25 +0500

    ;)

--- app.js
+++ app.js
@@ -65,29 +65,53 @@
 // }
 // console.log(f(12,8))
 
-function colculator () {
-    let num1 = prompt("birinchi raqamni kiriting")
-    let nums2 = prompt("ikkinchi raqamni kiriting")
-    let aperator = prompt("arifmetik amalni kiriting")
+// function colculator () {
+//     let num1 = prompt("birinchi raqamni kiriting")
+//     let nums2 = prompt("ikkinchi raqamni kiriting")
+//     let aperator = prompt("arifmetik amalni kiriting")
 
-    num1 = Number(num1)
-    nums2 = Number(nums2)
-    let answer;
+//     num1 = Number(num1)
+//     nums2 = Number(nums2)
+//     let answer;
 
-    if (aperator == "+") {
-   return answer = num1 + nums2;
-    }
-    else if (aperator == "-") {
-        return answer = num1 - nums2;
-    }
-    else if (aperator == "/") {
-        return answer = num1 / nums2;
-    }
-    else if (aperator == "*") {
-        return answer = num1 * nums2;
-    }
-    else {
-        alert ("xatolik");
-    }
-}
-alert(colculator());
+//     if (aperator == "+") {
+//    return answer = num1 + nums2;
+//     }
+//     else if (aperator == "-") {
+//         return answer = num1 - nums2;
+//     }
+//     else if (aperator == "/") {
+//         return answer = num1 / nums2;
+//     }
+//     else if (aperator == "*") {
+//         return answer = num1 * nums2;
+//     }
+//     else {
+//         alert ("xatolik");
+//     }
+// }
+// alert(colculator());
+
+
+const nums = [2, 4, 6, 8]
+console.log(nums.every((num) => num % 2 === 0))
+
+const users = ["tom", "john", "george"]
+console.log(users.find ((user) => user === "tom"))
+
+const ages = [10, 14, 18, 24];
+console.log(ages.some((age) => age == 18))
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers.reduce((a, b) => a+b))
+
+const fructis = ["apple", "banana", "cherry"];
+fructis.forEach((fructis, index) => {
+    console.log(index + 1 + ". " +fructis);
+});
+
+const numbers1= [1, 2, 4, 6, 8 ];
+console.log(numbers1.map((number) => number * number));
+
+const scores = [20, 30, 60, 80,];
+console.log(scores.filter((score) => score >= 50));
